a3537ac4dcc38ee7dabafc18e842357e2b0cf191
yvonneitan <itan3040@mylaurier.ca>
2024-10-30 09:50:11 -0700

    Delete some task lists

--- seeds/seed_users_and_task_lists.js
+++ seeds/seed_users_and_task_lists.js
@@ -21,12 +21,8 @@ export async function seed(knex) {
   // Insert multiple task lists using the userId
   const [
     workTasksListId,
-    personalTasksListId,
     shoppingListId,
     moviesListId,
-    destinationsListId,
-    fitnessTasksListId,
-    readingListId,
     financeListId,
     learningListId,
     travelPrepListId,
@@ -35,14 +31,6 @@ export async function seed(knex) {
       .insert({
         user_id: userId,
         name: "Work Tasks",
-        created_at: new Date(),
-      })
-      .then((result) => result[0]),
-
-    knex("task_lists")
-      .insert({
-        user_id: userId,
-        name: "Personal Tasks",
         created_at: new Date(),
       })
       .then((result) => result[0]),
@@ -63,52 +51,22 @@ export async function seed(knex) {
       })
       .then((result) => result[0]),
 
+   
     knex("task_lists")
       .insert({
         user_id: userId,
-        name: "Destinations",
+        name: "Finances",
         created_at: new Date(),
       })
       .then((result) => result[0]),
-      knex("task_lists")
-    .insert({
-      user_id: userId,
-      name: "Fitness Goals",
-      created_at: new Date(),
-    })
-    .then((result) => result[0]),
 
-  knex("task_lists")
-    .insert({
-      user_id: userId,
-      name: "Reading List",
-      created_at: new Date(),
-    })
-    .then((result) => result[0]),
-
-  knex("task_lists")
-    .insert({
-      user_id: userId,
-      name: "Finance",
-      created_at: new Date(),
-    })
-    .then((result) => result[0]),
-
-  knex("task_lists")
-    .insert({
-      user_id: userId,
-      name: "Learning",
-      created_at: new Date(),
-    })
-    .then((result) => result[0]),
-
-  knex("task_lists")
-    .insert({
-      user_id: userId,
-      name: "Travel Preparation",
-      created_at: new Date(),
-    })
-    .then((result) => result[0]),
+    knex("task_lists")
+      .insert({
+        user_id: userId,
+        name: "Travel Preparation",
+        created_at: new Date(),
+      })
+      .then((result) => result[0]),
   ]);
 
   // Insert multiple tasks for each list
@@ -150,35 +108,7 @@ export async function seed(knex) {
       end_time: new Date("2024-10-10 11:00:00"),
       status: "completed",
     },
-    {
-      user_id: userId,
-      list_id: personalTasksListId,
-      task: "Organize closet",
-      is_completed: true,
-      start_time: new Date("2024-10-12 15:00:00"),
-      end_time: new Date("2024-10-12 17:00:00"),
-      status: "completed",
-    },
-
-    // Personal Tasks
-    {
-      user_id: userId,
-      list_id: personalTasksListId,
-      task: "Prepare a low-carb lunch",
-      is_completed: false,
-      start_time: new Date("2024-10-17 12:00:00"),
-      end_time: new Date("2024-10-17 12:30:00"),
-      status: "active",
-    },
-    {
-      user_id: userId,
-      list_id: personalTasksListId,
-      task: "Watch Inception",
-      is_completed: false,
-      start_time: new Date("2024-10-16 18:00:00"),
-      end_time: new Date("2024-10-16 20:30:00"),
-      status: "active",
-    },
+ 
 
     // Shopping List
     {
@@ -208,7 +138,6 @@ export async function seed(knex) {
       end_time: new Date("2024-10-15 11:00:00"),
       status: "completed",
     },
-  
 
     // Movies List
     {
@@ -257,159 +186,44 @@ export async function seed(knex) {
       status: "active",
     },
 
-    // Destinations List
+    // Finance
     {
       user_id: userId,
-      list_id: destinationsListId,
-      task: "Visit Paris",
+      list_id: financeListId,
+      task: "Review monthly budget",
       is_completed: false,
-      start_time: new Date("2025-05-01 10:00:00"),
-      end_time: new Date("2025-05-07 18:00:00"),
+      start_time: new Date("2024-10-22 10:00:00"),
+      end_time: new Date("2024-10-22 11:00:00"),
       status: "active",
     },
     {
       user_id: userId,
-      list_id: destinationsListId,
-      task: "Trip to Tokyo",
+      list_id: financeListId,
+      task: "Pay credit card bill",
       is_completed: false,
-      start_time: new Date("2025-06-15 10:00:00"),
-      end_time: new Date("2025-06-30 18:00:00"),
+      start_time: new Date("2024-10-23 09:00:00"),
+      end_time: new Date("2024-10-23 09:30:00"),
+      status: "active",
+    },
+
+    // Travel Preparation
+    {
+      user_id: userId,
+      list_id: travelPrepListId,
+      task: "Pack luggage for weekend trip",
+      is_completed: false,
+      start_time: new Date("2024-10-26 15:00:00"),
+      end_time: new Date("2024-10-26 17:00:00"),
       status: "active",
     },
     {
       user_id: userId,
-      list_id: destinationsListId,
-      task: "Explore New York City",
-      is_completed: true,
-      start_time: new Date("2025-07-01 10:00:00"),
-      end_time: new Date("2025-07-15 18:00:00"),
-      status: "completed",
-    },
-    {
-      user_id: userId,
-      list_id: destinationsListId,
-      task: "See the Great Wall of China",
-      is_completed: true,
-      start_time: new Date("2024-10-03 08:00:00"),
-      end_time: new Date("2024-10-03 18:00:00"),
-      status: "completed",
-    },
-    {
-      user_id: userId,
-      list_id: destinationsListId,
-      task: "Go to the Grand Canyon",
+      list_id: travelPrepListId,
+      task: "Check-in for flight",
       is_completed: false,
-      start_time: new Date("2025-08-01 10:00:00"),
-      end_time: new Date("2025-08-05 18:00:00"),
+      start_time: new Date("2024-10-26 18:00:00"),
+      end_time: new Date("2024-10-26 18:30:00"),
       status: "active",
     },
-    {
-      user_id: userId,
-      list_id: destinationsListId,
-      task: "Visit Sydney",
-      is_completed: true,
-      start_time: new Date("2025-09-01 10:00:00"),
-      end_time: new Date("2025-09-10 18:00:00"),
-      status: "completed",
-    },
-    // Fitness Goals
-  {
-    user_id: userId,
-    list_id: fitnessTasksListId,
-    task: "Morning Yoga",
-    is_completed: false,
-    start_time: new Date("2024-10-17 06:30:00"),
-    end_time: new Date("2024-10-17 07:00:00"),
-    status: "active",
-  },
-  {
-    user_id: userId,
-    list_id: fitnessTasksListId,
-    task: "Run 5k",
-    is_completed: false,
-    start_time: new Date("2024-10-18 07:00:00"),
-    end_time: new Date("2024-10-18 08:00:00"),
-    status: "active",
-  },
-
-  // Reading List
-  {
-    user_id: userId,
-    list_id: readingListId,
-    task: "Read 'Atomic Habits'",
-    is_completed: false,
-    start_time: new Date("2024-10-17 19:00:00"),
-    end_time: new Date("2024-10-17 20:30:00"),
-    status: "active",
-  },
-  {
-    user_id: userId,
-    list_id: readingListId,
-    task: "Finish '1984' by George Orwell",
-    is_completed: false,
-    start_time: new Date("2024-10-20 19:00:00"),
-    end_time: new Date("2024-10-20 21:00:00"),
-    status: "active",
-  },
-
-  // Finance
-  {
-    user_id: userId,
-    list_id: financeListId,
-    task: "Review monthly budget",
-    is_completed: false,
-    start_time: new Date("2024-10-22 10:00:00"),
-    end_time: new Date("2024-10-22 11:00:00"),
-    status: "active",
-  },
-  {
-    user_id: userId,
-    list_id: financeListId,
-    task: "Pay credit card bill",
-    is_completed: false,
-    start_time: new Date("2024-10-23 09:00:00"),
-    end_time: new Date("2024-10-23 09:30:00"),
-    status: "active",
-  },
-
-  // Learning
-  {
-    user_id: userId,
-    list_id: learningListId,
-    task: "Complete JavaScript tutorial",
-    is_completed: false,
-    start_time: new Date("2024-10-24 14:00:00"),
-    end_time: new Date("2024-10-24 15:30:00"),
-    status: "active",
-  },
-  {
-    user_id: userId,
-    list_id: learningListId,
-    task: "Watch a TED Talk on productivity",
-    is_completed: false,
-    start_time: new Date("2024-10-25 12:00:00"),
-    end_time: new Date("2024-10-25 12:30:00"),
-    status: "active",
-  },
-
-  // Travel Preparation
-  {
-    user_id: userId,
-    list_id: travelPrepListId,
-    task: "Pack luggage for weekend trip",
-    is_completed: false,
-    start_time: new Date("2024-10-26 15:00:00"),
-    end_time: new Date("2024-10-26 17:00:00"),
-    status: "active",
-  },
-  {
-    user_id: userId,
-    list_id: travelPrepListId,
-    task: "Check-in for flight",
-    is_completed: false,
-    start_time: new Date("2024-10-26 18:00:00"),
-    end_time: new Date("2024-10-26 18:30:00"),
-    status: "active",
-  },
   ]);
 }
